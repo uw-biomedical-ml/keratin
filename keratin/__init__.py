1b657df7efcb698ed6c59cf88c3a7f060b98fe98
arokem <arokem@gmail.com>
2017-05-19 14:42:47 -0700

    Maybe this will make install properly?

--- keratin/__init__.py
+++ keratin/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .keratin import *  # noqa
-import keratin.networks  # noqa
+from .networks import * # noqa
