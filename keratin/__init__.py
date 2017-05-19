6022a3879a70810c00381bcddd90cd119b6732cf
arokem <arokem@gmail.com>
2017-05-19 14:37:19 -0700

    Fix import again.

--- keratin/__init__.py
+++ keratin/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .keratin import *  # noqa
-from keratin import networks  # noqa
+import keratin.networks  # noqa
